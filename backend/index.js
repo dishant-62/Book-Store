bc6a2ade1fdf0d1de69a01380189752a87a8b46b
Dishant Rathi <81504482+dishant-62@users.noreply.github.com>
2024-03-29 18:44:47 +0530

    Update index.js 12

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ app.use(express.json()); // this allows us to send any json file using client
 
 
   const db = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_DATABASE,
-    port: DB_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
   });
 
 
