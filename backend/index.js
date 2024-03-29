9e93eabdafbf2c87de2edc89e74b791a8ff9c3c3
Dishant Rathi <81504482+dishant-62@users.noreply.github.com>
2024-03-29 16:21:55 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json()); // this allows us to send any json file using client
     user: "root",
     password: "Krishna@2001",
     database: "Bookstore",
-    port: "3306"
+    port: "5000"
   });
 
 
